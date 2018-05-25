122f4fb9ac2f65604112d0d0ffd272fa32fb1aaa
lab developer <lab.desarrollo.java@gmail.com>
2018-05-25 18:00:38 +0200

    Primer commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { TasksTaskModule } from './task/task.module';
+import { TasksCategoryModule } from './category/category.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        TasksTaskModule,
+        TasksCategoryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
